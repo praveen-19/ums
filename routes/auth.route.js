ec4e852f0f82f00cdb57d3d39f0485b4652ed934
praveen-19 <praveenmahen19@gmail.com>
2022-09-15 14:27:56 +0530

    Validating user input

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/user.model");
+const { body, validationResult } = require("express-validator");
 
 router.get("/login", async (req, res, next) => {
   res.render("login");
@@ -10,31 +11,59 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.get("/register", async (req, res, next) => {
-  // req.flash("error", "some error");
-  // req.flash("error", "some error2");
-  // req.flash("info", "some value");
-  // req.flash("warning", "some value");
-  // req.flash("success", "some value");
-  // const messages = req.flash();
-  // res.redirect("/auth/login");
   res.render("register");
 });
 
-router.post("/register", async (req, res, next) => {
-  try {
-    const { email } = req.body;
-    const doesExist = await User.findOne({ email });
-    if (doesExist) {
-      res.redirect("/auth/register");
-      return;
+router.post(
+  "/register",
+  [
+    body("email")
+      .trim()
+      .isEmail()
+      .withMessage("Invalid Email")
+      .normalizeEmail()
+      .toLowerCase(),
+    body("password").trim().isLength(8).withMessage("Minimum 8 char required"),
+    body("password2").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password do not match");
+      }
+      return true;
+    }),
+  ],
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        errors.array().forEach((error) => {
+          req.flash("error", error.msg);
+        });
+        res.render("register", {
+          email: req.body.email,
+          messages: req.flash(),
+        });
+        return;
+      }
+
+      const { email } = req.body;
+      const doesExist = await User.findOne({ email });
+      if (doesExist) {
+        res.redirect("/auth/register");
+        return;
+      }
+      const user = new User(req.body);
+      await user.save();
+      req.flash(
+        "success",
+        `${user.email} registered successfully, you can login now`
+      );
+      res.redirect("/auth/login");
+      // res.send(user)
+    } catch (error) {
+      next(error);
     }
-    const user = new User(req.body);
-    await user.save();
-    res.send(user);
-  } catch (error) {
-    next(error);
   }
-});
+);
 
 router.get("/logout", async (req, res, next) => {
   res.send("Logout");
