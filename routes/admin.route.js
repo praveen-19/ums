6e31cfa79c49793cce85e9bdcd6281cc5679f89d
praveen-19 <praveenmahen19@gmail.com>
2022-09-16 11:24:46 +0530

    Managing users in UMS

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,10 +1,28 @@
 const User = require("../models/user.model");
 const router = require("express").Router();
+const mongoose = require("mongoose");
 
 router.get("/users", async (req, res, next) => {
   try {
     const users = await User.find();
-    res.send(users);
+    // res.send(users);
+    res.render("manage-users", { users });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/user/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      req.flash("error", "Invalid ID");
+      res.redirect("/admin/users");
+      return;
+    }
+    const person = await User.findById(id);
+    res.render("profile", { person });
   } catch (error) {
     next(error);
   }
